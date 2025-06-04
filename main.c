6163660a57382fe28edf39508240f25a6cbece21
shifat <shifat.dka@gmail.com>
2025-06-04 20:25:00 +0600

    Toggleled using Output compair channel 1, timer 2

--- main.c
+++ main.c
@@ -11,11 +11,11 @@ int main(void)
 
 	while (1)
 	{
-		if (!(TIM1->CR1 & TIM_CR1_CEN))
-		{
-			TIM1->CR1 |= TIM_CR1_CEN;
-			GPIOA->ODR ^= GPIO_ODR_ODR6;
-		}
+		// if (!(TIM1->CR1 & TIM_CR1_CEN))
+		//{
+		//	TIM1->CR1 |= TIM_CR1_CEN;
+		//	GPIOA->ODR ^= GPIO_ODR_ODR6;
+		//	}
 	}
 	return 0;
 }
@@ -23,22 +23,28 @@ int main(void)
 void Gpio_setup(void)
 {
 
-	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_AFIOEN;
 
 	// A6 as output
-	GPIOA->CRL &= ~(GPIO_CRL_CNF6 | GPIO_CRL_MODE6);
-	GPIOA->CRL |= GPIO_CRL_MODE6;
+	GPIOA->CRL &= ~(GPIO_CRL_CNF0 | GPIO_CRL_MODE0);
+	GPIOA->CRL |= GPIO_CRL_CNF0_1 | GPIO_CRL_MODE0;
 }
 
 void Timer_setup(void)
 {
-	RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
+	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 
-	TIM1->CNT = 0;
-	TIM1->CR1 |= TIM_CR1_DIR;// down counter
-	TIM1->CR1 |= TIM_CR1_OPM;
-	TIM1->PSC = 7200 - 1;
-	TIM1->ARR = 5000;
+	TIM2->CNT = 0;
+	TIM2->CR1 |= TIM_CR1_DIR; // down counter
+	// TIM1->CR1 |= TIM_CR1_OPM;
+	TIM2->PSC = 7200 - 1;
+	TIM2->ARR = 10000;
 
-	//TIM1->CR1 |= TIM_CR1_CEN;
+	TIM2->CCMR1 |= TIM_CCMR1_OC1M_0 | TIM_CCMR1_OC1M_1;
+
+	TIM2->CCR1 = 5000;
+
+	TIM2->CCER |= TIM_CCER_CC1E;
+
+	TIM2->CR1 |= TIM_CR1_CEN;
 }
