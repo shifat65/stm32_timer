65d7df4f159eda7e617e9f4d90073bbb809b5e89
shifat <shifat.dka@gmail.com>
2025-06-04 22:59:01 +0600

    Input capture

--- main.c
+++ main.c
@@ -11,11 +11,6 @@ int main(void)
 
 	while (1)
 	{
-		// if (!(TIM1->CR1 & TIM_CR1_CEN))
-		//{
-		//	TIM1->CR1 |= TIM_CR1_CEN;
-		//	GPIOA->ODR ^= GPIO_ODR_ODR6;
-		//	}
 	}
 	return 0;
 }
@@ -25,9 +20,9 @@ void Gpio_setup(void)
 
 	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_AFIOEN;
 
-	// A6 as output
+	// A0 as input pushpull
 	GPIOA->CRL &= ~(GPIO_CRL_CNF0 | GPIO_CRL_MODE0);
-	GPIOA->CRL |= GPIO_CRL_CNF0_1 | GPIO_CRL_MODE0;
+	GPIOA->CRL |= GPIO_CRL_CNF0_0;
 }
 
 void Timer_setup(void)
@@ -35,14 +30,15 @@ void Timer_setup(void)
 	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 
 	TIM2->CNT = 0;
-	//TIM2->CR1 |= TIM_CR1_DIR; // down counter
-	// TIM1->CR1 |= TIM_CR1_OPM;
+	// TIM2->CR1 |= TIM_CR1_DIR; // down counter
+	//  TIM1->CR1 |= TIM_CR1_OPM;
 	TIM2->PSC = 7200 - 1;
 	TIM2->ARR = 20000;
 
-	TIM2->CCMR1 |= TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1;// pwm 110
+	TIM2->CCMR1 |= TIM_CCMR1_CC1S_0; // cc1s= 01/ TI1 as input.
+	TIM2->CCMR1 |= TIM_CCMR1_IC1F;
 
-	TIM2->CCR1 = 15000;
+	//	TIM2->CCR1 = 15000;
 
 	TIM2->CCER |= TIM_CCER_CC1E;
 
